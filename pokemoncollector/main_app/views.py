49dafc524be80725c2cf8bb2920fa19ced5d8192
johnmccants002 <johnmccants002@gmail.com>
2022-05-17 23:39:25 -0700

    create update delete working

--- pokemoncollector/main_app/views.py
+++ pokemoncollector/main_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Pokemon
+
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def pokemons_index(request):
+    pokemons = Pokemon.objects.all()
+    return render(request, 'pokemons/index.html', {'pokemons': pokemons})
+
+def pokemons_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemons/detail.html', {'pokemon': pokemon})
+
+class PokemonCreate(CreateView):
+    model = Pokemon
+    fields = '__all__'
+    success_url = '/pokemons/'
+
+class PokemonUpdate(UpdateView):
+    model = Pokemon
+    fields = ["description"]
+    success_url = '/pokemons/'
+
+class PokemonDelete(DeleteView):
+    model = Pokemon
+    success_url = '/pokemons/'
